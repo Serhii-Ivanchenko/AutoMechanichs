73cf957a995abb94ec76846eb97097475edb9650
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2025-05-03 16:41:01 +0300

    create additional pages and routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,33 @@
-import { Navigate, Route, Router, Routes } from 'react-router-dom';
-import DiagnosticPage from '../../pages/DiagnosticPage/DiagnosticPage.jsx';
-import LoginPage from '../../pages/LoginPage/LoginPage.jsx';
-import MainPage from '../../pages/MainPage/MainPage.jsx';
-import PhotoCapturePage from '../../pages/PhotoCapturePage/PhotoCapturePage.jsx';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from '../Layout/Layout.jsx';
 import css from './App.module.css';
+
+import { lazy, Suspense } from 'react';
+
+const LoginPage = lazy(() => import('../../pages/LoginPage/LoginPage.jsx'));
+const MainPage = lazy(() => import('../../pages/MainPage/MainPage.jsx'));
+const AddCarPage = lazy(() => import('../../pages/AddCarPage/AddCarPage.jsx'));
+const PhotoCapturePage = lazy(() => import('../../pages/PhotoCapturePage/PhotoCapturePage.jsx'));
+const DiagnosticPage = lazy(() => import('../../pages/DiagnosticPage/DiagnosticPage.jsx'));
+const RepairPage = lazy(() => import('../../pages/RepairPage/RepairPage.jsx'));
+const NotFoundPage = lazy(() => import('../../pages/NotFoundPage/NotFoundPage.jsx'));
+
 
 export default function App() {
   return (
     <Layout>
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/main" element={<MainPage />} />
+          <Route path="/add-car" element={<AddCarPage />} />
           <Route path="/car/:carId/photos" element={<PhotoCapturePage />} />
           <Route path="/car/:carId/diagnostics" element={<DiagnosticPage />} />
+          <Route path="/car/:carId/repair" element={<RepairPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
+      </Suspense>
     </Layout>
   );
 }
---
+++ src/pages/RepairPage/RepairPage.jsx
@@ -0,0 +1,3 @@
+export default function RepairPage() {
+    return <div>RepairPage</div>
+};
