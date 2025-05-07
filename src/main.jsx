638792d6e1c83c8e7dad72b372b010206f78ff07
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2025-05-07 15:22:18 +0300

    create operations and slice for authorization

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,17 @@ import App from './components/App/App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import { persistor, store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <Provider store={store}> */}
-      {/* <PersistGate loading={null} persistor={persistor}> */}
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      {/* </PersistGate> */}
-    {/* </Provider> */}
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
