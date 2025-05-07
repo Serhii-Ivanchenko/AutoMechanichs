b15fde419478b9411b65d0bac404046d71bc5b4c
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2025-05-07 20:44:24 +0300

    fix styles

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -18,74 +18,74 @@ export default function LoginPage() {
 
   return (
     <div className={css.wrapper}>
-    <div className={css.page}>
-      <h1 className={css.header}>Вхід</h1>
-      <p className={css.text}>Welcome to Assist CONTROL</p>
+      <div className={css.page}>
+        <h1 className={css.header}>Вхід</h1>
+        <p className={css.text}>Welcome to Assist CONTROL</p>
 
-      <Formik
-        initialValues={{
-          login: '',
-          password: '',
-        }}
-        validationSchema={LoginSchema}
-        validateOnChange={true}
-        validateOnBlur={true}
-        onSubmit={handleSubmitLogin}
-      >
-        <Form className={css.form}>
-          <div className={css.inputWrapper}>
-            <label htmlFor="login" className={css.loginLabel}>
-              Логін*
-            </label>
-            <div className={css.inputWithIconWrapper}>
-              <BsFillPersonFill className={css.inputIcon} />
-              <Field
-                name="login"
-                type="text"
-                className={css.input}
-                placeholder="0733291544"
-              />
-            </div>
-            <ErrorMessage
-              name="login"
-              component="div"
-              className={css.errorMsg}
-            />
-          </div>
-          <div className={css.inputWrapper}>
-            <label htmlFor="password" className={css.loginLabel}>
-              Пароль*
-            </label>
-            <div className={css.inputWithIconWrapper}>
-              <BsKeyFill className={css.inputIcon} />
-              <Field
-                name="password"
-                type={isPasswordShown ? 'text' : 'password'}
-                className={css.input}
-                placeholder="password"
-              />
-              {isPasswordShown ? (
-                <ImEye className={css.eyeIcon} onClick={onButtonEyeClick} />
-              ) : (
-                <ImEyeBlocked
-                  className={css.eyeIcon}
-                  onClick={onButtonEyeClick}
+        <Formik
+          initialValues={{
+            login: '',
+            password: '',
+          }}
+          validationSchema={LoginSchema}
+          validateOnChange={true}
+          validateOnBlur={true}
+          onSubmit={handleSubmitLogin}
+        >
+          <Form className={css.form}>
+            <div className={css.inputWrapper}>
+              <label htmlFor="login" className={css.loginLabel}>
+                Логін*
+              </label>
+              <div className={css.inputWithIconWrapper}>
+                <BsFillPersonFill className={css.inputIcon} />
+                <Field
+                  name="login"
+                  type="text"
+                  className={css.input}
+                  placeholder="0733291544"
                 />
-              )}
+              </div>
+              <ErrorMessage
+                name="login"
+                component="div"
+                className={css.errorMsg}
+              />
             </div>
-            <ErrorMessage
-              name="password"
-              component="div"
-              className={css.errorMsg}
-            />
-          </div>
-          <p className={css.link}>Забули пароль?</p>
-          <button type="submit" className={css.submitBtn}>
-            Увійти
-          </button>
-        </Form>
-      </Formik>
+            <div className={css.inputWrapper}>
+              <label htmlFor="password" className={css.loginLabel}>
+                Пароль*
+              </label>
+              <div className={css.inputWithIconWrapper}>
+                <BsKeyFill className={css.inputIcon} />
+                <Field
+                  name="password"
+                  type={isPasswordShown ? 'text' : 'password'}
+                  className={css.input}
+                  placeholder="password"
+                />
+                {isPasswordShown ? (
+                  <ImEye className={css.eyeIcon} onClick={onButtonEyeClick} />
+                ) : (
+                  <ImEyeBlocked
+                    className={css.eyeIcon}
+                    onClick={onButtonEyeClick}
+                  />
+                )}
+              </div>
+              <ErrorMessage
+                name="password"
+                component="div"
+                className={css.errorMsg}
+              />
+            </div>
+            <p className={css.link}>Забули пароль?</p>
+            <button type="submit" className={css.submitBtn}>
+              Увійти
+            </button>
+          </Form>
+        </Formik>
       </div>
-      </div>
+    </div>
   );
 }
