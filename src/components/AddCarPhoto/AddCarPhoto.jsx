430e858dbe56c8177724f1daba4ed850211a0906
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2025-05-14 11:43:19 +0300

    update camera

--- src/components/AddCarPhoto/AddCarPhoto.jsx
+++ src/components/AddCarPhoto/AddCarPhoto.jsx
@@ -3,83 +3,35 @@ import { IoCloseCircle, IoCheckmarkCircleSharp } from 'react-icons/io5';
 import { BsCameraFill } from 'react-icons/bs';
 import { useCameraPhoto } from '../../utils/useCameraPhoto';
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
-
-// export default function AddCarPhoto({ setPhoto }) {
-//   const { handleTakePhoto, videoRef, canvasRef } = useCameraPhoto(photoData => {
-//     setPhoto(photoData);
-//   });
-
-//   return (
-//     <div className={css.wrapper}>
-//       <Link to="/main">
-//         <IoCloseCircle className={`${css.btn} ${css.cross}`} />
-//       </Link>
-
-//       <div>
-//         <video
-//           ref={videoRef}
-//           autoPlay
-//           playsInline
-//           style={{ width: 0, height: 0 }}
-//         />
-//         <canvas ref={canvasRef} style={{ display: 'none' }} />
-//         <button
-//           type="button"
-//           className={css.cameraBtn}
-//           onClick={handleTakePhoto}
-//         >
-//           <BsCameraFill className={css.cameraIcon} />
-//         </button>
-//       </div>
-//       <IoCheckmarkCircleSharp className={`${css.btn} ${css.check}`} />
-//     </div>
-//   );
-// }
 
 export default function AddCarPhoto({ setPhoto }) {
-  const {
-    startCamera,
-    stopCamera,
-    takePhoto,
-    isCameraActive,
-    videoRef,
-    canvasRef,
-  } = useCameraPhoto(photoData => {
+  const { handleTakePhoto, videoRef, canvasRef } = useCameraPhoto(photoData => {
     setPhoto(photoData);
   });
 
   return (
     <div className={css.wrapper}>
       <Link to="/main">
-        <IoCloseCircle
-          className={`${css.btn} ${css.cross}`}
-          onClick={stopCamera}
-        />
+        <IoCloseCircle className={`${css.btn} ${css.cross}`} />
       </Link>
 
-      {isCameraActive && (
+      <div>
         <video
           ref={videoRef}
           autoPlay
           playsInline
-          className={css.videoPreview}
+          style={{ width: 0, height: 0 }}
         />
-      )}
-      <canvas ref={canvasRef} style={{ display: 'none' }} />
-
-      {!isCameraActive ? (
-        <button type="button" className={css.cameraBtn} onClick={startCamera}>
+        <canvas ref={canvasRef} style={{ display: 'none' }} />
+        <button
+          type="button"
+          className={css.cameraBtn}
+          onClick={handleTakePhoto}
+        >
           <BsCameraFill className={css.cameraIcon} />
         </button>
-      ) : (
-        <IoCheckmarkCircleSharp
-          className={`${css.btn} ${css.check}`}
-          onClick={takePhoto}
-        />
-      )}
+      </div>
+      <IoCheckmarkCircleSharp className={`${css.btn} ${css.check}`} />
     </div>
   );
 }
-
-
