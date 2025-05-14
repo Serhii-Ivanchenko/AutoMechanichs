214ce3a8999657a8d406131649dbce287a29102d
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2025-05-14 12:05:28 +0300

    Update

--- src/components/AddCarPhoto/AddCarPhoto.jsx
+++ src/components/AddCarPhoto/AddCarPhoto.jsx
@@ -1,13 +1,54 @@
 import css from './AddCarPhoto.module.css';
 import { IoCloseCircle, IoCheckmarkCircleSharp } from 'react-icons/io5';
 import { BsCameraFill } from 'react-icons/bs';
-import { useCameraPhoto } from '../../utils/useCameraPhoto';
+// import { useCameraPhoto } from '../../utils/useCameraPhoto';
 import { Link } from 'react-router-dom';
+import { useRef, useState } from 'react';
 
-export default function AddCarPhoto({ setPhoto }) {
-  const { handleTakePhoto, videoRef, canvasRef } = useCameraPhoto(photoData => {
-    setPhoto(photoData);
-  });
+export default function AddCarPhoto({ setPhoto, setVideoStream }) {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [cameraOn, setCameraOn] = useState(false);
+
+  const startCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+      }
+      setVideoStream(stream);
+      setCameraOn(true);
+    } catch (err) {
+      console.error('Ошибка при доступе к камере', err);
+    }
+  };
+
+  const stopCamera = () => {
+    setCameraOn(false);
+    setVideoStream(null);
+    const tracks = videoRef.current?.srcObject?.getTracks();
+    tracks?.forEach(track => track.stop());
+  };
+
+  const takePhoto = () => {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    ctx.drawImage(video, 0, 0);
+    const dataUrl = canvas.toDataURL('image/jpeg');
+    setPhoto(dataUrl);
+    stopCamera();
+  };
+
+  const handleClick = () => {
+    if (!cameraOn) {
+      startCamera();
+    } else {
+      takePhoto();
+    }
+  };
 
   return (
     <div className={css.wrapper}>
@@ -20,14 +61,10 @@ export default function AddCarPhoto({ setPhoto }) {
           ref={videoRef}
           autoPlay
           playsInline
-          style={{ width: 0, height: 0 }}
+          style={{ display: 'none' }}
         />
         <canvas ref={canvasRef} style={{ display: 'none' }} />
-        <button
-          type="button"
-          className={css.cameraBtn}
-          onClick={handleTakePhoto}
-        >
+        <button type="button" className={css.cameraBtn} onClick={handleClick}>
           <BsCameraFill className={css.cameraIcon} />
         </button>
       </div>
