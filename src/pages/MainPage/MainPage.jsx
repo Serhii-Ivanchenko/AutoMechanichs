a7e17cfe9222ec7b0a393c61f025c8485fc03277
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2025-05-14 19:33:50 +0300

    responsive height (MainScreen, Layout)

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,5 @@
 import MainScreenSection from '../../components/MainScreenSection/MainScreenSection';
 
 export default function MainPage({ array1, array2, wage }) {
-  return (
-    <div>
-      <MainScreenSection array1={array1} array2={array2} wage={wage} />
-    </div>
-  );
+  return <MainScreenSection array1={array1} array2={array2} wage={wage} />;
 }
