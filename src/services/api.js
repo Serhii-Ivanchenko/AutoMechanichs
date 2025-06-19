ddd7d2297e2fdb238d769a0e464de15d2efcaaf1
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2025-06-19 16:28:25 +0300

    Added recogize vin&mileage photos

--- src/services/api.js
+++ src/services/api.js
@@ -12,8 +12,12 @@ export const clearAuthHeader = () => {
   axiosInstance.defaults.headers.common['X-Api-Key'] = '';
 };
 
-
 // Axios instance for Photos recognition
 export const axiosInstancePhotos = axios.create({
   baseURL: 'https://plate.assist.cam/',
+});
+
+// Axios instance for Vin and Mileage recognition
+export const axiosInstanceVinAndMileage = axios.create({
+  baseURL: 'https://aps.assist.cam/',
 });
